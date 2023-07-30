7e2fe1f85d394889b9276f778187e4835a7b7143
Benjamin-Riordan <>
2023-07-30 10:28:15 +0000

    add task function

--- run.py
+++ run.py
@@ -1,1 +1,5 @@
-print("hello world")
+# to do list 
+# Function to add a new task to the list
+def add_task(tasks, task_name):
+    tasks.append({"name": task_name, "done": False})
+    print(f"Task '{task_name}' added to the list!")
