29c72484de1a704ab3af59b3852d13f0ef8d0506
Benjamin-Riordan <>
2023-07-30 10:59:45 +0000

    alterd user messages to display correct info

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 # Function to add a new task to the list
-def add_task(tasks, task_name):
+def add_new_task(tasks, task_name):
     tasks.append({"name": task_name, "done": False})
     print(f"Task '{task_name}' added to the list!")
 
-def complete_task(tasks, task_index):
+def completed_task(tasks, task_index):
     if 1 <= task_index <= len(tasks):
         tasks[task_index - 1]["done"] = True
         print(f"Task '{tasks[task_index - 1]['name']}' marked as completed!")
     else:
         print("Invalid task index!")
-        
+
 # Function to display the current to-do list
-def display_tasks(tasks):
+def display_task_all(tasks):
     if not tasks:
         print("To-Do List is empty.")
     else:
@@ -30,19 +30,19 @@ def main():
         print("\nWhat do you want to do?")
         print("1. View To-Do List")
         print("2. Add Task")
-        print("3. Mark Task as Completed")
+        print("3. Mark Task as Completed( please view tasks before)")
         print("4. Exit")
 
         choice = input("Enter the number of your choice: ")
 
         if choice == "1":
-            display_tasks(tasks)
+            display_task_all(tasks)
         elif choice == "2":
             task_name = input("Enter the task name: ")
-            add_task(tasks, task_name)
+            add_new_task(tasks, task_name)
         elif choice == "3":
             task_index = int(input("Enter the task index to mark as completed: "))
-            complete_task(tasks, task_index)
+            completed_task(tasks, task_index)
         elif choice == "4":
             print("Exiting the To-Do List App. Goodbye!")
             break
