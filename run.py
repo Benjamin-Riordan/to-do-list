974d70c9583945f489a028e5ccae76298a52414f
Benjamin-Riordan <>
2023-07-30 10:29:47 +0000

    fucntion to mark a task as completed

--- run.py
+++ run.py
@@ -3,3 +3,11 @@
 def add_task(tasks, task_name):
     tasks.append({"name": task_name, "done": False})
     print(f"Task '{task_name}' added to the list!")
+
+# Function to mark a task as completed
+def complete_task(tasks, task_index):
+    if 1 <= task_index <= len(tasks):
+        tasks[task_index - 1]["done"] = True
+        print(f"Task '{tasks[task_index - 1]['name']}' marked as completed!")
+    else:
+        print("Invalid task index!")
